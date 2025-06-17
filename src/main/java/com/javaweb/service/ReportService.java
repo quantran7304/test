1b1a418f326320fe0f779262ba81924548b07583
lequandev <161694775+lequandev@users.noreply.github.com>
2025-06-17 07:19:02 +0700

    Delete src/main/java/com/javaweb/service/ReportService.java

--- src/main/java/com/javaweb/service/ReportService.java
+++
@@ -1,8 +0,0 @@
-package com.javaweb.service;
-
-import java.util.Map;
-
-public interface ReportService {
-    Map<String, Object> submitReport(Integer userId, Integer listingId, String reason, String comment);
-    String[] getReportReasons();
-}
