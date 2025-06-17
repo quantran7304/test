5974402f87def6b7586831753ca2fbf943a5c505
lequandev <161694775+lequandev@users.noreply.github.com>
2025-06-17 07:18:47 +0700

    Delete src/main/java/com/javaweb/service/DiscountService.java

--- src/main/java/com/javaweb/service/DiscountService.java
+++
@@ -1,7 +0,0 @@
-package com.javaweb.service;
-
-import java.util.Map;
-
-public interface DiscountService {
-    Map<String, Object> applyDiscountCode(Integer userId, String discountCode, Double totalAmount, Integer listingId);
-}
